7f05eef163a61bd099a0a60ff00824b47ed05ac5
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-06-11 23:20:40 +0300

    Finish Ui

--- Bmi Calculator/src/App.jsx
+++ Bmi Calculator/src/App.jsx
@@ -1,33 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [weight, setWight] = useState(0)
+
+  const [hight, setHight] = useState(0)
+
+  const [bmi, setBmi] = useState("")
+
+  const [msg, setMsg] = useState("")
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col justify-center items-center w-full h-screen">
+
+        <div className="rounded-lg p-4 shadow-lg">
+
+          <h1 className="text-center font-bold text-2xl mb-5">Bmi Calculator</h1>
+
+          <form action="">
+            <div>
+              <label>Weight in lbs</label>
+              <input className="w-full text-2xl py-2 px-1 rounded-lg border border-[#333]" type="number" value={weight} />
+            </div>
+
+            <div>
+              <label>Hight in inch</label>
+              <input className="w-full text-2xl py-2 px-1 rounded-lg border border-[#333]" type="number" value={hight} />
+            </div>
+
+            <div>
+              <button className="btn" type="submit">Submit</button>
+              <button className="btn bg-white text-[#333]" type="submit">Reset</button>
+            </div>
+
+          </form>
+
+          <div className="text-center my-[24px] mx-0">
+            <h2 className="text-center font-bold text-xl mb-5">Your Bmi is {bmi}</h2>
+            <p className="my-3 mx-0">{msg}</p>
+          </div>
+
+          <div className="text-center">
+            <img className="h-[200px]" src={state} alt="" />
+          </div>
+
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
